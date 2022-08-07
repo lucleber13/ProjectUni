e2ed765bb743785752ba33641bae5bc23973988b
lucleber13 <lucleber13@hotmail.com>
2022-08-07 18:04:08 +0100

    Create class User and update Access

--- src/main/java/com/cbcode/projectmentoria/model/Access.java
+++ src/main/java/com/cbcode/projectmentoria/model/Access.java
@@ -1,4 +1,66 @@
 package com.cbcode.projectmentoria.model;
 
-public class Access {
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "access")
+@SequenceGenerator(name = "seq_access", sequenceName = "seq_access", allocationSize = 1, initialValue = 1)
+public class Access implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_access")
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String description;
+
+    @Override
+    public String getAuthority() {
+        return this.description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Access other = (Access) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
 }
